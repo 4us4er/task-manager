38fb897b101532779d98d69c2245bef4414757a5
4us4e <145545210+4us4er@users.noreply.github.com>
2025-05-03 01:18:18 +0700

    feat: implement task adding logic and dynamic rendering

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,7 +1,23 @@
 import styles from './Task.module.css';
-
+import { useState } from 'react';
+import { ChangeEvent } from 'react';
 const Task = () => {
-  return <div className={styles.task}>Task 3</div>;
+  const [task,setTask]=useState('')
+  const handleInput = (evt:  ChangeEvent<HTMLInputElement>)=>{
+    const target = evt.target as HTMLButtonElement;
+    setTask(target.value);
+  }
+
+  return (
+  <label >
+    <input
+        name="myInput"
+        className={styles.task}
+        defaultValue="Some initial value"
+        onChange={handleInput}
+        value={task}
+    />
+</label>)
 };
 
 export { Task };
